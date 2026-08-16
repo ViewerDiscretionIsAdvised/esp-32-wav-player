f57403b5fcd512d294cc05183039651a674b7aba
ViewerDiscretionIsAdvised <christopher.kim541@gmail.com>
2026-08-16 00:16:16 -0700

    hello

---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "wav_player.h"
+
+void app_main(void) {
+    /*
+     * TODO: Write main.c
+     *
+     * - Configure the GPIO pins (3 needed) to be connected to I2S hardware peripheral
+     *   block, and configure I2S to stereo.
+     * - Use functions defined in wav_player.h to pass/play the .wav file.
+    */
+}
